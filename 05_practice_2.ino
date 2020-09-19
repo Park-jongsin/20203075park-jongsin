6152bbfcc8de7d5be8529e3dcc8070088271bd1f
Park-jongsin <66291287+Park-jongsin@users.noreply.github.com>
2020-09-20 04:37:35 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,51 @@
+#define PIN_LED 7
+int i = 0;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop()
+{
+  {
+    if(i==0)
+    {
+    digitalWrite(PIN_LED, 0);
+    delay(1000);
+    }
+    if (i < 5)
+    {
+    digitalWrite(PIN_LED, 1);
+    delay(100); 
+    digitalWrite(PIN_LED, 0);
+    delay(100);
+    i += 1;
+    }
+    if (i==5)
+    {
+    digitalWrite(PIN_LED, 1);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+void setup(){
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(PIN_LED, 1);
+  //digitalWrite(PIN_LED, 0);
+}
+*/
+
+ 
