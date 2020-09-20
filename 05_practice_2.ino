72c0afc99631ec231d0b02cc0f8a154e64e47f1c
Park-jongsin <66291287+Park-jongsin@users.noreply.github.com>
2020-09-20 19:39:50 +0900

    Add files via upload

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -6,26 +6,26 @@ void setup() {
 }
 
 void loop()
-{
-  {
-    if(i==0)
+{   while(i>=0)
     {
-    digitalWrite(PIN_LED, 0);
-    delay(1000);
+      if(i==0)
+      {
+        digitalWrite(PIN_LED, 0);
+        delay(1000);
+      }
+      if (i < 6)
+      {
+        digitalWrite(PIN_LED, 1);
+        delay(100); 
+        digitalWrite(PIN_LED, 0);
+        delay(100);
+        i += 1;
+      }
+      if (i==6)
+      {
+        digitalWrite(PIN_LED, 1);
+      }
     }
-    if (i < 5)
-    {
-    digitalWrite(PIN_LED, 1);
-    delay(100); 
-    digitalWrite(PIN_LED, 0);
-    delay(100);
-    i += 1;
-    }
-    if (i == 5)
-    {
-    digitalWrite(PIN_LED, 1);
-    }
-  }
 }
 
 
